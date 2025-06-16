86259152e8c25210fcb6f88f8c09df5469ed77d4
Sam <154955970+samps07@users.noreply.github.com>
2025-06-16 19:23:19 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();
+self.addEventListener('install', function(event) {
+  console.log('Service Worker Installed');
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  // Simple offline-first cache strategy can be added later
 });
